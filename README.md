4f2bd6755424fdb5c97ff0c6365ba483f47bfedb
Denley Hsiao <denley@justtodo.com>
2021-09-05 21:36:29 +0800

    Add cmake test content.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cmake_test
 cmake测试
+
+# 运行
+* `docker-compose up`
+* 最后一行出现`Successfully installed CMake XXX`内容即为正常
---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cmake --version
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc --all)
+./cmake_test
